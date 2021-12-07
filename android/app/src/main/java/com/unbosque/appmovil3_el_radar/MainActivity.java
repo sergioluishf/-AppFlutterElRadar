f3e87e38908c314a3fb89f3e190a12ac1d8a84de
Ing Sergio Herrera <sergioluishf@gmail.com>
2021-12-07 02:38:55 -0500

    Carga Inicial

---
+++ android/app/src/main/java/com/unbosque/appmovil3_el_radar/MainActivity.java
@@ -0,0 +1,6 @@
+package com.unbosque.appmovil3_el_radar;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'busqueda.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AppMovil_El_Radar',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home El Radar'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+
+              Padding(
+                padding:
+                EdgeInsets.only(left: 25, top: 25, right: 25, bottom: 0),
+                child: Center(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    child: Image.asset("image/logo.png"),
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding:
+                EdgeInsets.only(left: 25, top: 25, right: 25, bottom: 0),
+                child: TextField(
+                  //controller: buscar,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Búsqueda',
+                      hintText: 'Palabra clave de la búsqueda'
+                  ),
+                ),
+              ),
+
+              Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(minimumSize: Size(200, 45)),
+                    onPressed: () {
+                      print("Presionó el botón");
+                      //Navigator.push(context,
+                      // MaterialPageRoute(builder: (_) => busqueda(buscar.text)));
+                    },
+                    child: Text('Buscar'),
+                  )),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
